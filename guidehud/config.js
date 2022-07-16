1169137b898f1618065ea3d7cc36c6b07a31ec40
Swixx <ben768781@gmail.com>
2022-07-16 21:20:33 +0100

    [/] Guidehud and housing prices

--- guidehud/config.js
+++ guidehud/config.js
@@ -3,7 +3,7 @@ var config = {
   'welcomeMessage':'GMT Roleplay',
   // Add images for the loading screen background.
   images: [
-    'https://cdn.discordapp.com/attachments/972542378684592158/972578104226742272/home.png', 'https://cdn.discordapp.com/attachments/972542378684592158/972578104226742272/home.png', 'https://cdn.discordapp.com/attachments/972542378684592158/972578104226742272/home.png',  
+    '', '', '',  
   ],
 
   /*enableMusic: false,
@@ -17,7 +17,7 @@ var config = {
   // Change Discord settings and link.
   'discord': {
     'show': true,
-    'discordLink': 'https://discord.gg/GMT',
+    'discordLink': 'https://discord.io/gmtuk',
   },
   // Change which key you have set in guidehud/client/client.lua
   'menuHotkey': 'F1',
@@ -43,16 +43,15 @@ var announcements = [
 // Add/Modify rules below.
 var gettingstarted = [
   'Welcome to GMT',
-  'The world of GMT is pretty huge so it can be a little overwhelming at first! Here\'s a couple things you can do to get started and work you way up in the world:',
-  '',
+
   '1. Make sure you take the tutorial, where you first spawn in you need to follow a tutorial that\'ll take you to the Job Centre and introduce the jobs you can take.',
-  '',
+
   '2. Get a Job: A great starter job is Royal Mail Deliveries, Trucking or Bus Driving. To get started head to the Job Centre and select the job you wish to start. Next, open up your map and look for the Royal Mail/Bus/Trucking start point and drive there using your taxi that you recieved when doing the tutorial.',
-  '',
+
   '3. Buy your first car at Simeons, look on the map if you don\'t know where Simeons Dealership is. Once you purchase the vehicle, you\'ll be able to take it out from any garage across the map free of charge.',  
-  '',
+ 
   '4. Try out the life of crime, there are many benefits the criminal life offers, you may start/join a gang(F5), rob people, rob shops, banks and even do illegal drug runs.',
-  '',
+
   '5. As you progress through the server, you will need to buy licenses to access certain jobs & garages, see the License centre at the Job centre',
   '6. Try the many fun activies you can take part in the server! From skateboarding, paintball to parachuting!',
   '7. Go explore our world, create endless RP opportunities and create friendships that\'ll last forever :)',

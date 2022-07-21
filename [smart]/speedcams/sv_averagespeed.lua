bfb194fe84eefdec78c0533c2f32a754f38fd402
GMT <107719738+GMT-Development@users.noreply.github.com>
2022-07-21 04:34:15 +0100

    /

--- [smart]/speedcams/sv_averagespeed.lua
+++ [smart]/speedcams/sv_averagespeed.lua
@@ -14,10 +14,29 @@ AddEventHandler("Server:AverageSpeedDetection",function(cameraId, speed, roadNam
     -- Add permission checks here or link in with your database for vRP / ESX
 end)
 
-
-    
-
-
+RegisterNetEvent("GMT:FinePlayer")
+AddEventHandler("GMT:FinePlayer",function(playerfined,fineamount)
+    local source = source
+    local officer_id = vRP.getUserId(source)
+    local user_id = vRP.getUserId(playerfined)
+    local player_id = vRP.getUserSource(playerfined)
+    if vRP.hasPermission(user_id,"police.armoury") then
+        vRPclient.notify(source,{'~r~You cannot fine another officer'})
+        return
+    end
+    if not vRP.hasPermission(user_id, "police.armoury") and vRP.hasPermission(officer_id, "police.armoury") then
+        if tonumber(vRP.getBankMoney(user_id)) > tonumber(fineamount*5000) then
+            vRP.setBankMoney(user_id,tonumber(vRP.getBankMoney(user_id))-tonumber(fineamount*5000))
+            vRP.setBankMoney(officer_id,tonumber(vRP.getBankMoney(officer_id))+tonumber(fineamount*5000))
+            vRPclient.notify(playerfined,{'~r~You have been issued a speeding fine of £'..(fineamount*5000)..' for going '..fineamount.."MPH over the speed limit."})
+            vRPclient.notify(source,{'~r~You issused a speeding fine '..GetPlayerName(playerfined)..' £'..(fineamount*5000)..' for going '..fineamount.."MPH over the speed limit."})
+            TriggerClientEvent('GMT:PlayerFined', playerfined)
+        else
+            vRPclient.notify(playerfined,{"~r~You have been issued a fine for speeding"})
+            vRPclient.notify(source,{"~r~This player dosnt have enough money"}) 
+        end
+    end
+end)   
 
 function mathRound(value, numDecimalPlaces)
     if numDecimalPlaces then

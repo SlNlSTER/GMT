c45f2891d6d1b7cc62e315d26ccdf1e4c4de551a
Swixx <ben768781@gmail.com>
2022-07-29 16:43:34 +0100

    [/] Tips made ooc

--- GMT-Core/client/cl_tips.lua
+++ GMT-Core/client/cl_tips.lua
@@ -1,48 +1,19 @@
-GMTTips = { 
-    "Watch out, there is more recoil than usual in this city",
-   -- "Support GMT @store.gmt.city for some cool VIP perks!",
-   -- "Support GMT @store.gmt.city for some cool VIP perks!",
-    --"Support GMT @store.gmt.city for some cool VIP perks!",
-    --"Support GMT @store.gmt.city for some cool VIP perks!",
-    "Press L to open your inventory",
-    "KOS is only allowed at redzones!",
-    "You can point with B",
-    "You can make your minimap bigger with Z",
-    "You can perform CPR on your dead friends, with a small chance of resuscitation using /cpr",
-    "You sell all legal goods (Copper,Gold etc..) at the Trader which is south of the map near the docks",
-    "You can get your GP to take a look at you and restore your health at any Hospital",
-    -- "Check out our Website for whitelisted faction applications, https://www.GMTForums.net",
-    -- "Want to join the PD? Apply at https://www.GMTForums.com",
-    "Use /ooc or // to ask out of character questions",
-    "To call an admin, type /calladmin",
-    --"To report a player you can create a player report at https://www.GMTForums.com/forums/",
-    "You can lock your car with the comma key [,]",
-    "If you are experiencing texture loss set your Texture Quality to Normal in graphics settings!",
-    "F6 to see your licenses",
-    "F5 to see your gang menu",
-    "F10 to see your past warnings/kicks/bans",
-    "M for vehicle functions/control",
-    "Join our discord for discussion & development news https://discord.io/GMTUK",
-    "Join our discord for discussion & development news https://discord.io/GMTUK",
-    "Join our discord for discussion & development news https://discord.io/GMTUK",
-    "Join our discord for discussion & development news https://discord.io/GMTUK",
-    --"Register on our website for discussion and whitelisting applications https://www.GMTForums.net",
-    "Press F1 for help on getting started, controls & rules",
-    "Press F1 for help on getting started, controls & rules",
-    "Press F1 for help on getting started, controls & rules",
-    "Press F1 for help on getting started, controls & rules",
-    "Remember, selling or advertising the sale of anything in out of character chat is not allowed!",
-    "Remember, selling or advertising the sale of anything in out of character chat is not allowed!",
-    "Remember, selling or advertising the sale of anything in out of character chat is not allowed!",
-    "Remember, selling or advertising the sale of anything in out of character chat is not allowed!",
-    "Remember, selling or advertising the sale of anything in out of character chat is not allowed!",
-   -- "For an ingame reward of £500,000 follow our twitter @GMTFiveM!",
-   -- "For an ingame reward of £500,000 follow our twitter @GMTFiveM!",
-   --"For an ingame reward of £500,000 follow our twitter @GMTFiveM!",
-   -- "For an ingame reward of £500,000 follow our twitter @GMTFiveM!",
-   -- "For an ingame reward of £500,000 follow our twitter @GMTFiveM!",
-   -- "For an ingame reward of £500,000 follow our twitter @GMTFiveM!",
-   -- "For an ingame reward of £500,000 follow our twitter @GMTFiveM!"
+prefix = '^1[GMT]^7 '
+
+GMTTips = 
+{ 
+    prefix.."You can view your Licenses by pressing (F6)",
+    prefix.."The redzones located on the map are KOS",
+    prefix.."You can point with (B)",
+    prefix.."You can access your inventory with (L)",
+    prefix.."You can lock your vehicle using (,)",    
+    prefix.."Use /ooc or // to ask out of character questions",
+    prefix.."To call an admin, type /calladmin",
+    prefix.."You can lock your car with the comma key",
+    prefix.."If you are experiencing texture loss set your Texture Quality to Normal in graphics settings!",
+    prefix.."Press (F10) to see your past punishments",
+    prefix.."For vehicle functions press (M)",
+    prefix.."If you need support with anything do /calladmin",
 }
 
 
@@ -51,7 +22,7 @@ Citizen.CreateThread(function()
     while true do
         math.randomseed(GetGameTimer())
         num = math.random(1,#GMTTips)
-        TriggerEvent("chatMessage", "^1[GMT Tips]^1  " .. "^5" .. GMTTips[num] .. "^5","alert")
+        TriggerEvent('chatMessage',"", {255, 51, 51}, "" .. GMTTips[num], "ooc")
         Wait(600000)
     end
 end)

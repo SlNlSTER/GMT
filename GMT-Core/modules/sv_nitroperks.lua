246206443a2de606c01c06061df7faf996971ab8
Swixx <ben768781@gmail.com>
2022-08-02 10:11:48 +0100

    ** [ + ] ** Add logs to the nitro thing

--- GMT-Core/modules/sv_nitroperks.lua
+++ GMT-Core/modules/sv_nitroperks.lua
@@ -8,12 +8,37 @@ AddEventHandler('NitroPerks:craftmoped', function()
             Citizen.Wait(3500)
             print('[NitroPerks] ' .. name .. ' has crafted a moped')
             TriggerClientEvent("NitroPerks:spawnmoped", source)
+            TriggerEvent("NitroPerks:logtodiscord", userid, name)
         else
             TriggerClientEvent("GMT:PlaySound", source, 2)
-            TriggerClientEvent("NitroPerks:notbooster", source)
+            TriggerClientEvent("NitroPerks:notbooster", userid, name)
             print('^1[NitroPerks] ' .. name .. ' tried triggering an event the cheating scum')
         end
     end)
 end)
 
 
+            ["fields"] = {
+                {
+                    ["name"] = "User Name",
+                    ["value"] = name,
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "User ID",
+                    ["value"] = userid,
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "Event",
+                    ["value"] = "Crafted a moped",
+                    ["inline"] = true
+                },
+                
+
+            }
+        }
+    }
+    local webhook = "https://discord.com/api/webhooks/1003786571142680700/KPluP_-wKiSrbN8k6WNUsh9uw1dHIBjcQHd56JZPcw-ODJs529AqjFbKhc-YlzUlguUN"
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = "GMT", embeds = command}), { ['Content-Type'] = 'application/json' })
+end)
